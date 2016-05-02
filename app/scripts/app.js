5e12c26a4ac341898bc32416f8d5595610aea076
mikeyjcat <mike@hewitson.co.za>
2016-05-02 07:03:39 +0200

    Remove contact and about us pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,11 +23,6 @@ angular
                 controller: 'MainCtrl',
                 controllerAs: 'main'
             })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl',
-                controllerAs: 'about'
-            })
             .when('/history', {
                 templateUrl: 'views/history.html',
                 controller: 'HistoryChartCtrl',
